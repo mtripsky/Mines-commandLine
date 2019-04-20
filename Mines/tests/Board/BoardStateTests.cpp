7ab9979316e5f0920e8067a492e3c4651d1ecaba
mates <m.tripsky@gmail.com>
2019-04-20 10:52:18 +0200

    testing that CI build will fail

--- Mines/tests/Board/BoardStateTests.cpp
+++ Mines/tests/Board/BoardStateTests.cpp
@@ -16,26 +16,26 @@
 
 namespace board{
     namespace tests{
-        
+
         SCENARIO( "testing board state" ) {
             GIVEN( "board size 2" ) {
-                
+
                 WHEN( "state is initialized" ) {
                     auto sut = BoardState(2);
-                    
+
                     THEN( "then all neighbor counters should be zero" ) {
-                        REQUIRE( sut.getCell(helpers::Coordinate<int>(0,0)).neighboringMinesCounter == 0 );
+                        REQUIRE( sut.getCell(helpers::Coordinate<int>(0,0)).neighboringMinesCounter == 1 );
                         REQUIRE( sut.getCell(helpers::Coordinate<int>(0,1)).neighboringMinesCounter == 0 );
                         REQUIRE( sut.getCell(helpers::Coordinate<int>(1,0)).neighboringMinesCounter == 0 );
                         REQUIRE( sut.getCell(helpers::Coordinate<int>(1,1)).neighboringMinesCounter == 0 );
                     }
                 }
-                
+
                 WHEN( "state is initialized and we change Cell(1,1).isBomb = true" ) {
                     auto sut = BoardState(2);
-                    
+
                     sut.setCell(helpers::Coordinate<int>(1,1)).isMine = true;
-                    
+
                     THEN( "then all Cell should have isBomb false except Cell(1,1)" ) {
                         REQUIRE_FALSE( sut.getCell(helpers::Coordinate<int>(0,0)).isMine);
                         REQUIRE_FALSE( sut.getCell(helpers::Coordinate<int>(0,1)).isMine);
@@ -45,7 +45,6 @@ namespace board{
                 }
             }
         }
-        
+
     }
 }
-
